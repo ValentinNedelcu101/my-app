fc26bef47bb6f357e3b72b77b8892d3c2aa411ff
Valentin <valentin.nedelcu@apprenant.ifapme.be>
2026-01-14 13:58:38 +0100

    Component interaction btw product-list and product-detail: click and add events

---
+++ src/app/product-detail/product-detail.ts
@@ -0,0 +1,15 @@
+import { Component, input, output } from '@angular/core';
+import { Product } from '../product';
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.css',
+})
+export class ProductDetail {
+  product = input<Product>();
+  added = output();
+  addToCart(){
+    this.added.emit();
+  }
+}
---
+++ src/app/product-list/product-list.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Product } from '../product';
+import { ProductDetail } from '../product-detail/product-detail';
+@Component({
+  selector: 'app-product-list',
+  imports: [ProductDetail],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductList {
+  products: Product[]= [
+    { id:1, title: "Keyboard"},
+    { id:2, title: "Microphone"},
+    { id:3, title: "Web camera"},
+    { id:4, title: "Tablet"}
+  ];
+  selectedProduct:Product |undefined;
+  onAdded(){
+    alert(`${this.selectedProduct?.title} added to the cart`)
+  }
+}
