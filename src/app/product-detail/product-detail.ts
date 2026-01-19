9915843ad6cbfa60f78131bae651b35c3b388f9c
Valentin <valentin.nedelcu@apprenant.ifapme.be>
2026-01-19 11:07:20 +0100

    Chapter 4: Applying pipes

--- src/app/product-detail/product-detail.ts
+++ src/app/product-detail/product-detail.ts
@@ -1,15 +1,33 @@
-import { Component, input, output } from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, output, ViewEncapsulation, OnChanges, SimpleChanges, } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '../product';
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.html',
   styleUrl: './product-detail.css',
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class ProductDetail {
+export class ProductDetail implements OnChanges {
   product = input<Product>();
   added = output();
   addToCart(){
     this.added.emit();
   }
+  get productTitle(){
+    return this.product()!.title;
+  }
+  constructor(){
+    console.log('Product:', this.product());
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    const product = changes['product'];
+    if(!product.isFirstChange()){
+    const oldValue = product.previousValue;
+    const newValue = product.currentValue;
+    console.log('Old value', oldValue);
+    console.log('New value', newValue);
+  }
+}
+
 }
