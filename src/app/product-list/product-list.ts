d5963af4b7ae6ddc9ac922dc6b842412ebfe1a9e
Valentin <valentin.nedelcu@apprenant.ifapme.be>
2026-01-20 12:58:16 +0100

    Using services and dependency injection to fetch products

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Product } from '../product';
 import { ProductDetail } from '../product-detail/product-detail';
 import { SortPipe } from '../sort-pipe';
+import { ProductsService } from '../products.service';
 @Component({
   selector: 'app-product-list',
   imports: [ProductDetail, SortPipe],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {
-  products: Product[]= [
-    { id:1, title: "Keyboard", price:100, categories: {1:"Computing", 2:"Peripherals"}},
-    { id:2, title: "Microphone", price:35, categories: {3: "Multimedia"}},
-    { id:3, title: "Web camera", price: 79, categories: {1: "Computing", 3:"Multimedia"}},
-    { id:4, title: "Tablet", price: 500, categories: {4: "Entertainment"}}
-  ];
+export class ProductList implements OnInit {
+  products: Product[]= [];
   selectedProduct:Product |undefined =this.products[0];
   onAdded(){
     alert(`${this.selectedProduct?.title} added to the cart`)
+  }
+  private productService = inject(ProductsService);
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
   }
 }
