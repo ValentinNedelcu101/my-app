c8ddeb71635be6001b04799b35187a4664616ad8
Valentin <valentin.nedelcu@apprenant.ifapme.be>
2026-01-27 12:32:42 +0100

    Overriding service implementation

--- src/app/favorites/favorites.ts
+++ src/app/favorites/favorites.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
-import { CommonModule } from '@angular/common';
+import { FavoritesService } from '../favorites-service';
 @Component({
   selector: 'app-favorites',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
+  providers: [{
+    provide: ProductsService,
+    useClass: FavoritesService
+  }],
 })
 export class Favorites implements OnInit {
 products : Product[] = [];
 
-constructor(private productsService: ProductsService) {}
+constructor( private productsService: ProductsService) {}
 ngOnInit(): void {
   this.products = this.productsService.getProducts();
 }
