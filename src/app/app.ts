e974b7a59e7750d2e036954fd75f44d4a41d9ad3
Valentin <valentin.nedelcu@apprenant.ifapme.be>
2026-01-27 12:50:25 +0100

    Transforming objects (app.settings.ts) in Angular services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductList } from "./product-list/product-list";
 import { Copyright } from './copyright';
 import { Numeric } from './numeric';
-
+import { APP_SETTINGS, appSettings } from './app.settings';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ProductList, Copyright, Numeric],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [
+    {provide: APP_SETTINGS, useValue: appSettings}
+  ]
 })
 export class App {
   protected readonly title = signal('World');
   description = "Hello World";
+  settings = inject(APP_SETTINGS);
 }
